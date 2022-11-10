e53e3fa30f6f1c4eee79578387e1b8852dd15651
Ivan <ivanbrazas@gmail.com>
2022-11-09 22:58:02 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import type {Product} from "./types";
+const [products, setProducts] = useState<Product[]>([]);
+const [query, setQuery] = useState<string>("");
 
-import {useEffect, useState} from "react";
+useEffect(() => {
+  api.search(query).then(setProducts);
 
-import api from "./api";
+}, [query]);
 
-function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [query, setQuery] = useState<string>("");
 
-  useEffect(() => {
-    api.search(query);
-  }, [query]);
+useEffect(() => {
+  api.search().then(setProducts)
 
-  return (
-    <main>
-      <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+}, []) 
+console.log(products[0]?.price <= 100)
+if(products?.length === 0) return <h1>Cargando</h1>
+
+return (
+  <main>
+    <h1>Tienda digitaloncy</h1>
+    <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+    <ul>
+      {products.map((product) => (
+        <li className={`${product.price <= 100 ? "sale" : ""} `} key={product.id}>
+          <h4>{product.title}</h4>
+          <p>{product.description}</p>
+          <span>$ {product.price}</span>
+        </li>
+      ))}
+    </ul>
+  </main>
+);
 }
 
 export default App;
